94a13a12f63e3d985fd9955bb1a355e119a486f3
Victoria Melendez <vamelend@gmail.com>
2016-07-11 12:42:12 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -54,10 +54,10 @@ end
 
 def runner
   welcome
-  hand=initial_round
-until hand>21 do
-   hand=hit?(hand)
- display_card_total(hand)
+  card_total=initial_round
+until card_total>21 do
+   card_total=hit?(card_total)
+ display_card_total(card_total)
   end
- end_game(hand)
+ end_game(card_total)
 end
